b1fd4eed1e902cc3981c44eb9d6e0fa95c1968db
chand796 <kalluri.chandana@diggibyte.com>
2024-03-13 17:59:02 +0530

    assignment 3 commited

--- src/assignment3/driver.py
+++ src/assignment3/driver.py
@@ -1,4 +1,4 @@
-from python_assignments.src.assignment3.util import mutate_string
+from Python_assignmen.src.assignment3.util import mutate_string
 s = input()
 i, c = input().split()
 s_new = mutate_string(s, int(i), c)
