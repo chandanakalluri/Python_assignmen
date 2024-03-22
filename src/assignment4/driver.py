5ed3f36c3564af77b10cc973b10527d3b26f4a99
chandanakalluri <kalluri.chandana@diggibyte.com>
2024-03-22 14:25:54 +0530

    adding charecter in a string in a given index

--- src/assignment4/driver.py
+++ src/assignment4/driver.py
@@ -1,5 +1,4 @@
-from python_assignments.src.assignment4.util import merge_the_tools
+from Python_assignmen.src.assignment4.util import merge_the_tools
 
 if __name__ == '__main__':
-    string, k = "ssdssfdde", 3
-    merge_the_tools(string, k)
+    print(merge_the_tools())
--- src/assignment4/util.py
+++ src/assignment4/util.py
@@ -1,12 +1,21 @@
-def merge_the_tools(string, k):
-    # Divide the string into parts of length k
-    parts = [string[i:i + k] for i in range(0, len(string), k)]
-    for part in parts:
-        unique_chars = []
+# def merge_the_tools(string, k):
+#     # Divide the string into parts of length k
+#     parts = [string[i:i + k] for i in range(0, len(string), k)]
+#     for part in parts:
+#         unique_chars = []
+#
+#         for char in part:
+#             if char not in unique_chars:
+#                 unique_chars.append(char)
+#
+#         # Print the unique characters without duplicates
+#         print(' '.join(unique_chars))
+def merge_the_tools():
+    s=input()
+    s1=int(input())
+    k=input()
+    li=[]
+    return(s[:s1]+k+s[s1+1:])
 
-        for char in part:
-            if char not in unique_chars:
-                unique_chars.append(char)
 
-        # Print the unique characters without duplicates
-        print(' '.join(unique_chars))
+
