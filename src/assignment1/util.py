ada68a708e991468e1fd51ea2dbe372ccbba0f8c
chandanakalluri <kalluri.chandana@diggibyte.com>
2024-03-22 14:12:39 +0530

    finding average of student marks

--- src/assignment1/util.py
+++ src/assignment1/util.py
@@ -1,12 +1,17 @@
-def cal_avg(n, student_data, query_name):
-    student_marks = {}
-    for _ in range(n):
-        name, *line = student_data[_].split()
-        scores = list(map(float, line))
-        student_marks[name] = scores
-
-    if query_name in student_marks:
-        avg = sum(student_marks[query_name]) / len(student_marks[query_name])
-        return "{:.2f}".format(avg)
-
+def cal_avg():
+    # student_marks = {}
+    # for _ in range(n):
+    #     name, *line = student_data[_].split()
+    #     scores = list(map(float, line))
+    #     student_marks[name] = scores
+    #
+    # if query_name in student_marks:
+    #     avg = sum(student_marks[query_name]) / len(student_marks[query_name])
+    #     return "{:.2f}".format(avg)
+    n = int(input())
+    student_dat = input().split()
+    name = int(input())
+    if name==student_dat[0]:
+        print(student_dat[1:])
+cal_avg()
 
